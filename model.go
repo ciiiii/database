3821ae139323b5f88ac1e535a14a8db553426e23
ciiiii <allen.ccccnm@gmail.com>
2020-01-10 14:07:32 +0800

    adjust model

--- model.go
+++ model.go
@@ -1,13 +1,13 @@
 package database
 
 type Service struct {
-	ID       uint   `gorm:"primary_key"`
+	ID       uint64 `gorm:"type:bigint(20) unsigned auto_increment;not null;primary_key"`
 	Name     string `gorm:"type:varchar(255)"`
-	Host     string `json:"host,omitempty";gorm:"type:varchar(255)"`
-	Port     int    `json:"port,omitempty";gorm:"type:integer"`
-	Priority int    `json:"priority,omitempty";gorm:"type:integer"`
-	Weight   int    `json:"weight,omitempty";gorm:"type:integer"`
-	Text     string `json:"text,omitempty";gorm:"type:varchar(255)"`
-	Mail     bool   `json:"mail,omitempty";gorm:"type:boolean"`
-	TTL      uint32 `json:"mail,omitempty";gorm:"type:integer"`
+	Host     string `gorm:"type:varchar(15)"`
+	Port     int    `gorm:"type:integer"`
+	Priority int    `gorm:"type:integer"`
+	Weight   int    `gorm:"type:integer"`
+	Text     string `gorm:"type:varchar(255)"`
+	Mail     bool   `gorm:"type:boolean"`
+	TTL      uint32 `gorm:"type:integer"`
 }
